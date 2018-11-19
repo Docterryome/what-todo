6217094980517f770ac94f012405510b4d1433f1
Docterryome <docterryome@gmail.com>
2018-11-18 23:42:13 -0600

    Fragment the mustache templates for better maitinence

--- src/test/kotlin/com/docterryome/todo/toDoListapp/IntegrationTest.kt
+++ src/test/kotlin/com/docterryome/todo/toDoListapp/IntegrationTest.kt
@@ -1,8 +1,11 @@
 package com.docterryome.todo.toDoListapp
 
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
 
-@ExtendWith(SpringExtension::class)
+@RunWith(SpringRunner::class)
+@SpringBootTest
 class IntegrationTest(@Autowired val restTemplate: TestRestTemplate)
